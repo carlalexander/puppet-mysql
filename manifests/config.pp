9288ab85d8891625fce607c063568dac26ac9acc
Carl Alexander <carlalexander@gmail.com>
2013-02-10 16:13:32 -0500

    Added some additional security defaults

--- manifests/config.pp
+++ manifests/config.pp
@@ -16,11 +16,18 @@
 class mysql::config (
   $root_password = undef
 ) inherits mysql::params {
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+  }
   File {
     owner => 'root',
     group => 'root',
     mode  => '0644',
   }
+
+  if ($root_password == undef or $root_password == '') {
+    fail('You must set a root password')
+  } 
 
   file { $mysql::config::backup_dir:
     ensure => directory,
@@ -36,10 +43,20 @@ class mysql::config (
     require => File[$mysql::config::conf_dir]
   }
 
-  if ($root_password != undef and $root_password != '') {
-    exec { 'set-mysql-root-password':
-      unless  => "/usr/bin/mysqladmin status -uroot -p${root_password}",
-      command => "/usr/bin/mysqladmin -uroot password ${root_password}",
-    }
+  exec { 'mysql-set-root-password':
+    command => "mysqladmin -uroot password ${root_password}",
+    unless  => "mysqladmin status -uroot -p${root_password}",
+  }
+
+  exec { 'mysql-drop-test-database'
+    command     => "mysql -uroot -p${root_password} -e \"DROP DATABASE test;\"",
+    refreshonly => true,
+    subscribe   => Exec['mysql-set-root-password'],
+  }
+
+  exec { 'mysql-disable-anonymous-user'
+    command     => "mysql -uroot -p${root_password} -e \"DROP USER ''@localhost;\"",
+    refreshonly => true,
+    subscribe   => Exec['mysql-set-root-password'],
   }
 }
